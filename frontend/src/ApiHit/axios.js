ab82a30c83dc8ab1f13fd9f766530801ebabc893
Umesh Shrestha <98099220+Umesh341@users.noreply.github.com>
2025-11-20 20:32:05 +0545

    fix: Update axios instance baseURL for improved environment handling

--- frontend/src/ApiHit/axios.js
+++ frontend/src/ApiHit/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE == "production" ? "https://guizbackend.onrender.com"  : 'http://localhost:8000',
+    baseURL: import.meta.env.MODE === "production" ? "https://guizbackend.onrender.com/api" : 'http://localhost:8000/api',
     withCredentials: true,
     headers: { "Content-Type": "application/json" }
     
