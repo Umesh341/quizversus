b4738bf3bd3480c5ac83623a20b31c160af2ebf3
Umesh Shrestha <98099220+Umesh341@users.noreply.github.com>
2025-11-20 20:25:49 +0545

    fix: Correct baseURL for local development in axios instance

--- frontend/src/ApiHit/axios.js
+++ frontend/src/ApiHit/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE == "production" ? import.meta.env.BACKEND_URL  : 'http://localhost:8000/api',
+    baseURL: import.meta.env.MODE == "production" ? "https://guizbackend.onrender.com"  : 'http://localhost:8000',
     withCredentials: true,
     headers: { "Content-Type": "application/json" }
     
