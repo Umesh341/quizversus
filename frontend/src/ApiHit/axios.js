4389c510b82aef7f9d8ecd12451b7ab13f714268
Umesh Shrestha <98099220+Umesh341@users.noreply.github.com>
2025-11-20 20:36:07 +0545

    fix: Update axios baseURL to use BACKEND_URL from environment variables

--- frontend/src/ApiHit/axios.js
+++ frontend/src/ApiHit/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-
-
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "production" ? "https://guizbackend.onrender.com/api" : 'http://localhost:8000/api',
+    baseURL: import.meta.env.MODE === "production" ? import.meta.env.BACKEND_URL + "/api" : "http://localhost:8000/api",
     withCredentials: true,
     headers: { "Content-Type": "application/json" }
     
